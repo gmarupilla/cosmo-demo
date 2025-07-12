d8305b6b24c308a11d4a6377b27cf509e48dcd23
marupilla <marupilla@mail.com>
2025-07-12 16:24:49 -0500

    Add extra data and container build docs

--- subgraph-users/data.ts
+++ subgraph-users/data.ts
@@ -2,9 +2,13 @@ export const publishers = [
   {
     id: "dc",
     name: "DC Comics",
+    foundedYear: 1934,
+    totalComicsPublished: 12000,
   },
   {
     id: "marvel",
     name: "Marvel Comics",
+    foundedYear: 1939,
+    totalComicsPublished: 32000,
   },
 ];
